6e0cf57c6462c5a3370da658c6085b9417abdc0a
elisa.roger <e26.roger@gmail.com>
2024-01-19 10:43:22 +0100

    modif sur les dossiers

--- Javascript/Evenement/Exercice 7/script.js
+++
@@ -1,6 +0,0 @@
-// Ecouter la page
-
-let bar = document.querySelector('.bar');
-let scrollMax = document.body.scrollHeight - window.innerHeight;
-let percentage = window.scrollY / scrollMax * 100;
-bar.style.width = `${percentage}%`;
